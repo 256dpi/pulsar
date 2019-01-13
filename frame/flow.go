f89f6f835da69661df0c884c7266a88f4a61d1ab
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-01-13 21:27:04 +0100

    added flow

--- frame/flow.go
+++ frame/flow.go
@@ -1,1 +1,28 @@
 package frame
+
+import (
+	"github.com/256dpi/pulsar/pb"
+
+	"github.com/golang/protobuf/proto"
+)
+
+type Flow struct {
+	CID            uint64
+	MessagePermits uint32
+}
+
+func (f *Flow) Encode() (*pb.BaseCommand, error) {
+	// prepare flow command
+	flow := &pb.CommandFlow{
+		ConsumerId:     proto.Uint64(f.CID),
+		MessagePermits: proto.Uint32(f.MessagePermits),
+	}
+
+	// prepare base command
+	base := &pb.BaseCommand{
+		Type: getType(pb.BaseCommand_FLOW),
+		Flow: flow,
+	}
+
+	return base, nil
+}
