738873f94b4d0ed7e104e280fd6951266f41ca17
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-01-13 14:01:32 +0100

    renamed package to pb

--- pb/update.sh
+++ pb/update.sh
@@ -7,7 +7,7 @@ wget -q -O pulsar.proto https://raw.githubusercontent.com/apache/pulsar/v2.2.1/p
 clang-format -style="{BasedOnStyle: llvm, ColumnLimit: 120}" -i pulsar.proto
 
 # prepend package name
-sed -i '' -e 's/message Schema {/option go_package = \"pp\";\
+sed -i '' -e 's/message Schema {/option go_package = \"pb\";\
 message Schema {/' pulsar.proto
 
 # generate go file
