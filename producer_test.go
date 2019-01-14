248039074631b38a93050add115b785afc148d00
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-01-14 23:38:30 +0100

    added benchmark

--- producer_test.go
+++ producer_test.go
@@ -1,6 +1,7 @@
 package pulsar
 
 import (
+	"sync/atomic"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -29,4 +30,45 @@ func TestProducer(t *testing.T) {
 
 	err = producer.Close()
 	assert.NoError(t, err)
+}
+
+func BenchmarkProducer(b *testing.B) {
+	b.ReportAllocs()
+
+	producer, err := CreateProducer(ProducerConfig{
+		Topic: "test",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	msg := ProducerMessage{
+		Payload: []byte("hello"),
+	}
+
+	done := make(chan struct{})
+
+	var counter int64
+
+	for i := 0; i < b.N; i++ {
+		err = producer.Send(msg, func(cbMsg ProducerMessage, cbErr error) {
+			if cbErr != nil {
+				panic(cbErr)
+			}
+
+			if int(atomic.AddInt64(&counter, 1)) == b.N {
+				close(done)
+			}
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	safeWait(done)
+
+	err = producer.Close()
+	if err != nil {
+		panic(err)
+	}
 }
