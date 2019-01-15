46a3b4635d14f13ad521109580bb6bf87cc3123b
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-01-15 15:17:36 +0100

    properly delete all topics

--- reset.sh
+++ reset.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+# enable echo
+set -x
+
 # set java home
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+
+# remove all topics
+pulsar-admin topics list public/test | xargs -t -L1 pulsar-admin topics delete -f
 
 # remove namespace
 pulsar-admin namespaces delete public/test
 
 # create namespace
-pulsar-admin namespaces create --clusters standalone public/test
+pulsar-admin namespaces create -c standalone public/test
