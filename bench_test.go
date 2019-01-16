3fcaa66e5283d8b8720f3e7f2673b7c8cbe77dbe
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-01-16 11:05:35 +0100

    added benchmarks

--- bench_test.go
+++ bench_test.go
@@ -26,6 +26,8 @@ func BenchmarkProducer(b *testing.B) {
 
 	var counter int64
 
+	b.ResetTimer()
+
 	for i := 0; i < b.N; i++ {
 		err = producer.Send(msg, func(_ ProducerMessage, err error) {
 			if err != nil {
@@ -44,6 +46,76 @@ func BenchmarkProducer(b *testing.B) {
 	safeWait(lastSendAck)
 
 	err = producer.Close()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func BenchmarkConsumer(b *testing.B) {
+	b.ReportAllocs()
+
+	queue := make(chan ConsumerMessage, 100)
+
+	consumer, err := CreateConsumer(ConsumerConfig{
+		Topic:                    "public/test/bench2",
+		Subscription:             "bench2",
+		MessageCallback: func(msg ConsumerMessage) {
+			queue <- msg
+		},
+		ErrorCallback: func(err error) {
+			panic(err)
+		},
+	})
+
+	producer, err := CreateProducer(ProducerConfig{
+		Topic: "public/test/bench2",
+		ErrorCallback: func(err error) {
+			panic(err)
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	msg := ProducerMessage{
+		Payload: []byte("bench2"),
+	}
+
+	for i:=0; i<b.N; i++ {
+		err = producer.Send(msg, func(_ ProducerMessage, err error) {
+			if err != nil {
+				panic(err)
+			}
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	err = consumer.Flow(b.N)
+	if err != nil {
+		panic(err)
+	}
+
+	b.ResetTimer()
+
+	counter := 0
+
+	for {
+		msg := <-queue
+
+		err = consumer.AckCumulative(msg.ID)
+		if err != nil {
+			panic(err)
+		}
+
+		counter++
+		if counter == b.N {
+			break
+		}
+	}
+
+	err = consumer.Close()
 	if err != nil {
 		panic(err)
 	}
